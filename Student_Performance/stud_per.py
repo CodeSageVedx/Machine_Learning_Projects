d8399d88d1571ade52807b762681bdc2693f31cc
Vedant Kanoje <141636261+CodeSageVedx@users.noreply.github.com>
2025-03-23 16:30:33 +0530

    Update stud_per.py

--- Student_Performance/stud_per.py
+++ Student_Performance/stud_per.py
@@ -5,7 +5,7 @@ import pickle
 from sklearn.preprocessing import StandardScaler,LabelEncoder
 
 def load_model():
-    with open("strudent_performance_lr_finalmodel.pkl","rb") as file:
+    with open("Student_Performance/strudent_performance_lr_finalmodel.pkl","rb") as file:
         model,scaler,le = pickle.load(file)
     return model,scaler,le
 
